0135ff3d4ae2150a35507a0f465694e5184c5b98
mantr002 <rotenda.mantsha@vcontractor.co.za>
2024-04-11 17:32:01 +0200

    Adding DotNetEnv Dependency

--- CodeLearningSpectaclesAPI/CodeLearningSpectaclesAPI/Program.cs
+++ CodeLearningSpectaclesAPI/CodeLearningSpectaclesAPI/Program.cs
@@ -11,21 +11,21 @@ namespace CodeLearningSpectaclesAPI
     {
       var builder = WebApplication.CreateBuilder(args);
 
-      // Add services to the container.
-
+            // Add services to the container.
+      DotNetEnv.Env.Load();
       builder.Services.AddControllers();
       // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
       builder.Services.AddEndpointsApiExplorer();
       builder.Services.AddSwaggerGen();
       builder.Services.AddDbContext<CodeLearningDbContext>(options => options.UseNpgsql(
-           "Server=" + "code-learning-postgres-db.c7klvipobgy8.eu-west-1.rds.amazonaws.com"
-           + ";Port=" + "5432"
-           + ";User Id=" + "CodeLearningSpectacles"
-           + ";Password=" + "topsecretpassword"
-           + ";Database=" + "codeLearningDB" + ";"
+        "Server=" + Environment.GetEnvironmentVariable("SERVER")
+        + ";Port=" + Environment.GetEnvironmentVariable("PORT")
+        + ";User Id=" + Environment.GetEnvironmentVariable("USER_ID")
+        + ";Password=" + Environment.GetEnvironmentVariable("PASSWORD")
+        + ";Database=" + Environment.GetEnvironmentVariable("DATABASE") + ";"
       ));
 
-      var app = builder.Build();
+            var app = builder.Build();
 
       // Configure the HTTP request pipeline.
       if (app.Environment.IsDevelopment())
@@ -39,11 +39,11 @@ namespace CodeLearningSpectaclesAPI
       app.UseAuthorization();
 
       //// Include authentication middleware for api
-      app.UseWhen(x => (x.Request.Path.StartsWithSegments("/api", StringComparison.OrdinalIgnoreCase)),
-            builder =>
-            {
-                builder.UseMiddleware<RequestFilter>();
-            });
+      //app.UseWhen(x => (x.Request.Path.StartsWithSegments("/api", StringComparison.OrdinalIgnoreCase)),
+      //      builder =>
+      //      {
+      //          builder.UseMiddleware<RequestFilter>();
+      //      });
 
             app.MapControllers();
 
